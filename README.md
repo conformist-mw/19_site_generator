c1b87b9c85d6ef96f10b4ac78218c667954417e6
conformist-mw <conformist.mw@gmail.com>
2016-11-15 17:40:14 +0200

    stage 4

--- README.md
+++ README.md
@@ -1,1 +1,17 @@
-# 19_site_generator
+# 19 Site generator
+
+Данный скрипт может конвертировать файлы с разметкой markdown в html и создаёт статический сайт с базовой вёрсткой (применяется bootstrap). [Здесь](https://conformist-mw.github.io/19_site_generator/site/index.html) можно ознакомиться с примером такого сайта. 
+
+Структура сайта задаётся конфиг-файлом в формате json, его нужно указать при запуске скрипта. 
+
+## Запуск скрипта
+
+Традиционно для запуска нужно установить необходимые зависимости, которые перечислены в файле requirements.txt, например так:
+
+:::python
+pip3 install -r requirements.txt
+:::
+И после этого:
+:::bash
+python3 site_generator.py config.json
+:::
--- site_generator.py
+++ site_generator.py
@@ -3,20 +3,21 @@ import json
 import os
 from jinja2 import Environment, FileSystemLoader
 from collections import defaultdict
+import argparse
 BASE_DIR = 'site/articles'
 
 
-def load_json(file):
+def load_data(file):
     with open(file, 'r') as f:
         return json.load(f)
 
 
-def mk_html(file):
+def convert_md_to_html(file):
     with open(file, 'r') as f:
         return markdown(f.read(), extensions=['codehilite', 'fenced_code'])
 
 
-def render_page(data):
+def render_base_page(data):
     env = Environment(loader=FileSystemLoader('./'))
     template = env.get_template('templates/base.html')
     return template.render(data)
@@ -29,27 +30,31 @@ def mk_site_struct(config):
         filename = filename.replace('md', 'html')
         if not os.path.exists(outdir):
             os.makedirs(outdir)
-        html = mk_html(os.path.join('articles', item['source']))
+        html = convert_md_to_html(os.path.join('articles', item['source']))
         data = {'html': html,
                 'title': item['title'],
                 'topic': item['topic']}
         with open(os.path.join(outdir, filename), 'w') as f:
-            f.write(render_page(data))
+            f.write(render_base_page(data))
 
 
 def mk_site_index(config):
-    d = defaultdict(list)
+    data = defaultdict(list)
     for item in config['articles']:
         path = item['source'].replace('md', 'html')
-        d[item['topic']].append([os.path.join('articles', path),
-                                item['title']])
+        data[item['topic']].append([os.path.join('articles', path),
+                                    item['title']])
     env = Environment(loader=FileSystemLoader('./'))
     template = env.get_template('templates/index.html')
     with open('site/index.html', 'w') as f:
-        f.write(template.render(struct=d))
+        f.write(template.render(struct=data))
 
 
 if __name__ == '__main__':
-    struct = load_json('config.json')
+    parser = argparse.ArgumentParser(
+        description='Make static site from markdown files')
+    parser.add_argument('filepath', help='json file')
+    args = parser.parse_args()
+    struct = load_data(args.filepath)
     mk_site_struct(struct)
     mk_site_index(struct)
